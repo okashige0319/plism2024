2f5e66af146251fd945729d5b6e51ecb25737aea
okashige0319 <okashige@os.rim.or.jp>
2024-11-11 13:06:06 +0900

    Initial Commit

---
+++ Plism2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Plism2024
+//
+//  Created by Shigeru Okada on 2024/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Plism2024/Plism2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Plism2024App.swift
+//  Plism2024
+//
+//  Created by Shigeru Okada on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct Plism2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
